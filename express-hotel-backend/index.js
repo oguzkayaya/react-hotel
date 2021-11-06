cd49bc771fc0f9ecf52fe0c3367c3dc8f569c360
oguz kaya <oguz.kaya@etstur.com>
2021-11-06 22:09:26 +0300

    connect to db, register, login

--- express-hotel-backend/index.js
+++ express-hotel-backend/index.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 
-app.liste(3000, () => {
+
+dotenv.config();
+
+app.use(express.json());
+app.use("/api/user", authRoute);
+
+mongoose.connect(
+    process.env.DB_CONNECT,
+    () => {
+        console.log("connected to db");
+    }
+)
+
+app.listen(3000, () => {
     console.log("Server up and running");
 })
---
+++ express-hotel-backend/routes/auth.js
@@ -0,0 +1,36 @@
+const User = require("../models/User");
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+
+router.post("/register", async (req, res) => {
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+    });
+
+    try {
+        const savedUser = await user.save();
+        res.send({ id: user._id, name: user.name, email: user.email });
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+router.post("/login", async (req, res) => {
+
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send("Email is not found");
+
+    const isPasswordValid = await bcrypt.compare(req.body.password, user.password);
+    if(!isPasswordValid) return res.status(400).send("Invalid password");
+
+    res.send("Login successfully");
+})
+
+module.exports = router;
+
