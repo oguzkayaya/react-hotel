6606979a94c78ad7506a6f33395f26e26dbcc4c3
oguz kaya <oguz.kaya@etstur.com>
2021-11-23 17:10:10 +0300

    express, hotel save, hotel list

--- express-hotel-backend/routes/hotels.js
+++ express-hotel-backend/routes/hotels.js
@@ -1,9 +1,27 @@
 const router = require("express").Router();
 const verify = require("../verifyToken");
+const Hotel = require("../models/Hotel");
 
-router.get("/", verify, (req,res) => {
-    res.json(req.user);
+router.get("/", verify, async (req, res) => {
+    try {
+        const hotels = await Hotel.find({});
+        res.json(hotels);
+    } catch (error) {
+        return res.status(400).send(error);
+    }
+})
 
+router.post("/", verify, async (req, res) => {
+    try {
+        const newPost = new Hotel({
+            name: req.body.hotelName,
+        });
+        const savedHotel = await newPost.save();
+        return res.json({ savedHotel });
+    } catch (error) {
+        return res.status(400).send(error);
+
+    }
 })
 
 
